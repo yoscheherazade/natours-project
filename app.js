791994d5c349bba24ab1e212d948ffdaec4aab1c
LaQuita <lgollman@wayne.edu>
2018-06-23 13:02:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// TODO this is a todo comment
+// ? this is an alert
+// ! No, this is an alert
+// ? I guess this is for questions?
+// * This is a highlight
+// This is a normal comment
+
+/**
+// This is a strikethrough comment
+* TODO This is a todo within a multi-line comment
+* ? This is a question
+* ! This is an alert
+* * This is a highlight
+*/
